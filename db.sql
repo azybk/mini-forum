236b23b03b610f8ac6ab65bf3d75a6b9057d2bfa
azy bk <kumbaraazy@gmail.com>
2025-03-16 14:13:20 +0700

    db user activities

--- db.sql
+++ db.sql
@@ -25,3 +25,16 @@ CREATE TABLE IF NOT EXISTS comments(
     CONSTRAINT fk_post_id_comments FOREIGN KEY (post_id) REFERENCES posts(id),
     CONSTRAINT fk_user_id_comments FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE IF NOT EXISTS user_activities(
+    id int AUTO_INCREMENT PRIMARY key,
+    post_id int not null,
+    user_id bigint not null,
+    is_liked boolean not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    created_by text NOT NULL,
+    updated_by text NOT NULL,
+    CONSTRAINT fk_post_id_user_activities FOREIGN KEY (post_id) REFERENCES posts(id),
+    CONSTRAINT fk_user_id_user_activities FOREIGN KEY (user_id) REFERENCES users(id)
+);
